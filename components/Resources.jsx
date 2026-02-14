10d762448d2379fb7dc795c29e2736fc7e9c1d07
Mirela Dragulescu <dragulescumirela@gmail.com>
2026-02-14 11:16:26 +0100

    added a new section for articles in Resources and fixed the css

--- components/Resources.jsx
+++ components/Resources.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "@styles/Resources.css";
+import LeadershipImg from "../src/assets/Leadership.jpg";
+import AIImg from "../src/assets/artificial-intelligence.jpeg";
+import ChangeManagementImg from "../src/assets/change-management.jpeg";
+import CoachingImg from "../src/assets/Coaching.jpeg";
+import EIImg from "../src/assets/emotional-intelligence.jpeg";
+import FinanceImg from "../src/assets/finance.png";
+import PerformanceImg from "../src/assets/performance.jpeg";
 
 export function Resources() {
   const navigate = useNavigate();
@@ -8,12 +15,65 @@ export function Resources() {
     navigate(-1);
   };
 
-  const resourceCards=[
-    {title:"Leadership", route:"/resources/leadership"},
-    {title:"Performance", route:"/resources/performance"},
-    {title:"Coaching", route:"/resources/coaching"},
-    {title:"Change Management", route:"/resources/change-management"},
-    {title:"Emotional Intelligence", route:"/resources/emotional-intelligence"},
+  const resourceCards = [
+    { title: "Leadership", route: "/resources/leadership" },
+    {
+      title: "Artificial Intelligence",
+      route: "/resources/artificial-intelligence",
+    },
+    { title: "Finance", route: "/resources/finance" },
+    { title: "Performance", route: "/resources/performance" },
+    { title: "Coaching", route: "/resources/coaching" },
+    { title: "Change Management", route: "/resources/change-management" },
+    {
+      title: "Emotional Intelligence",
+      route: "/resources/emotional-intelligence",
+    },
+  ];
+
+  const articleCards = [
+    {
+      title: "Leadership",
+      articleTitle: "Article Title",
+      route: "/resources/leadership/article-1",
+      image: LeadershipImg,
+    },
+    {
+      title: "Artificial Intelligence",
+      articleTitle: "Article Title",
+      route: "/resources/artificial-intelligence/article-1",
+      image: AIImg,
+    },
+    {
+      title: "Finance",
+      articleTitle: "Article Title",
+      route: "/resources/finance/article-1",
+      image: FinanceImg,
+    },
+    {
+      title: "Performance",
+      articleTitle: "Article Title",
+      route: "/resources/performance/article-1",
+      image: PerformanceImg,
+    },
+    {
+      title: "Coaching",
+      articleTitle: "Article Title",
+      route: "/resources/coaching/article-1",
+      image: CoachingImg,
+    },
+    {
+      title: "Change Management",
+      articleTitle: "Article Title",
+      route: "/resources/change-management/article-1",
+      image: ChangeManagementImg,
+    },
+    {
+      title: "Emotional Intelligence",
+      articleTitle: "Article Title",
+      route: "/resources/emotional-intelligence/article-1",
+      image: EIImg,
+    },
   ];
   return (
     <div className="resources">
@@ -22,14 +82,39 @@ export function Resources() {
       </button>
       <h1>People Development Hub</h1>
       <h2>
-      Welcome to the centralized hub of learning resources, aimed to help you shape your skills</h2>
+        Welcome to the centralized hub of learning resources, aimed to help you
+        shape your skills
+      </h2>
       <div className="cards-container">
-        {resourceCards.map((card)=>(
-          <div key={card.title} className="resource-card" onClick={()=>navigate(card.route)}>
-        {card.title}
+        {resourceCards.map((card) => (
+          <div
+            key={card.title}
+            className="resource-card"
+            onClick={() => navigate(card.route)}
+          >
+            {card.title}
+          </div>
+        ))}
       </div>
-        ))} 
-    </div>
+      <div className="resources-cards-container">
+        {articleCards.map((card) => (
+          <div
+            key={card.articleTitle}
+            className="resource-medium-card"
+            onClick={() => navigate(card.route)}
+          >
+            <div className="card-content">
+              <span className="card-category">{card.title}</span>
+              <h3 className="card-article-title">{card.articleTitle}</h3>
+            </div>
+            <img
+              src={card.image}
+              alt={card.articleTitle}
+              className="card-image"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
